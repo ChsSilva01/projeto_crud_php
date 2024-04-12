7c3c799ed25c660a7140927f239ccfb3ca9de8dc
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2024-04-12 11:07:17 -0300

    1.0

---
+++ view/index.php
@@ -0,0 +1,79 @@
+<?php include("blades/top.php"); ?>
+
+<div class="container mt-5">
+    <a href="v_cadastro.php" class="btn btn-primary">Cadastar</a>
+</div>
+
+<div class="container mt-2 bg-white p-3 rounded">
+    <table class="table table-bordered table-hover table-striped">
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Nome</th>
+                <th>Cidade</th>
+                <th>Curso</th>
+                <th colspan="2">Ação</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>01</td>
+                <td>João Da Silva</td>
+                <td>Registro</td>
+                <td>TDS</td>
+                <td><a href="#" class="btn btn-success">Editar</a></td>
+                <td><a href="#" class="btn btn-danger">Excluir</a></td>
+            </tr>
+            <tr>
+                <td>01</td>
+                <td>João Da Silva</td>
+                <td>Registro</td>
+                <td>TDS</td>
+                <td><a href="#" class="btn btn-success">Editar</a></td>
+                <td><a href="#" class="btn btn-danger">Excluir</a></td>
+            </tr>
+            <tr>
+                <td>01</td>
+                <td>João Da Silva</td>
+                <td>Registro</td>
+                <td>TDS</td>
+                <td><a href="#" class="btn btn-success">Editar</a></td>
+                <td><a href="#" class="btn btn-danger">Excluir</a></td>
+            </tr>
+            <tr>
+                <td>01</td>
+                <td>João Da Silva</td>
+                <td>Registro</td>
+                <td>TDS</td>
+                <td><a href="#" class="btn btn-success">Editar</a></td>
+                <td><a href="#" class="btn btn-danger">Excluir</a></td>
+            </tr>
+            <tr>
+                <td>01</td>
+                <td>João Da Silva</td>
+                <td>Registro</td>
+                <td>TDS</td>
+                <td><a href="#" class="btn btn-success">Editar</a></td>
+                <td><a href="#" class="btn btn-danger">Excluir</a></td>
+            </tr>
+            <tr>
+                <td>01</td>
+                <td>João Da Silva</td>
+                <td>Registro</td>
+                <td>TDS</td>
+                <td><a href="#" class="btn btn-success">Editar</a></td>
+                <td><a href="#" class="btn btn-danger">Excluir</a></td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <td colspan="6">Rodapé da tabela</td>
+        </tfoot>
+    </table>
+    <!-- <form action="">
+        <label>Nome</label>
+        <input type="text">
+        <button></button>
+    </form> -->
+</div>
+
+<?php include("blades/footer.php"); ?>
---
+++ view/v_cadastro.php
@@ -0,0 +1,28 @@
+<?php include("blades/top.php"); ?>
+
+<div class="container mt-5">
+    <!-- "/" voltar para index -->
+    <a href="index.php" class="btn btn-primary">Voltar</a>
+</div>
+
+<div class="container mt-2 bg-white p-3 rounded">
+    <form action="" class="forms">
+        <label class="form-label">Nome:</label>
+        <input type="text" class="form-control">
+
+        <label class="form-label pt-3">Cidade:</label>
+        <input type="text" class="form-control">
+        
+        <label class="form-label pt-3">Curso:</label>
+        <input type="text" class="form-control ">
+        <br>
+        <a href="" class="btn btn-primary">Salvar</a>
+    </form>
+    <!-- <form action="">
+        <label>Nome</label>
+        <input type="text">
+        <button></button>
+    </form> -->
+</div>
+
+<?php include("blades/footer.php"); ?>
