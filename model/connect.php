6082cfc536653b5875bb1484cdeb8d0daaa9adba
Carlos Henrique <105667538+ChsSilva01@users.noreply.github.com>
2024-09-19 13:28:13 -0300

    a

--- model/connect.php
+++ model/connect.php
@@ -1,5 +1,8 @@
 <?php
+    //extabelecemos um conexão com o banco de dados
     $conexao = mysqli_connect('127.0.0.1','root','');
+    //selecionamos a database que vamos utilizar
     mysqli_select_db($conexao,'projeto_crud_php_t1');
+    //definimos a linguagem como português
     mysqli_set_charset($conexao,'UTF8');
 ?>
