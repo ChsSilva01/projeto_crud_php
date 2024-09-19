a946379947350482c32c80b36879c4f792d9601a
Carlos Henrique <105667538+ChsSilva01@users.noreply.github.com>
2024-09-19 13:12:19 -0300

    a

--- controller/funcao-create-aluno.php
+++ controller/funcao-create-aluno.php
@@ -1,19 +1,26 @@
 <?php
+    //incluindo a conexão com o banco de dados
     include("../model/connect.php");
 
+    //
     $arquivo = $_FILES["arquivo"];
+    //
     $file_name = $_FILES['arquivo']['name'];
+    //estamos guardando em uma váriavel a extenção da nossa imagem
     $extensao = pathinfo($file_name, PATHINFO_EXTENSION);
+    //agora estamos fazendo um nome aleatório, criptografando e adicionando sua extensão
     $nomeCompleto = md5(uniqid(pow(date("s"),date("m")))) . "." . $extensao;
-  
+
+    //salvamos o caminho da imagem em um variável
     $destino = "../view/imgs/".$nomeCompleto;
+    //agora movemos a imagem com o nome alterado para o seu destino
     move_uploaded_file($arquivo['tmp_name'],$destino);
-    //Variável global, variáveis do php
+    //pelo query executamos um comando SQL
     mysqli_query($conexao, "INSERT INTO alunos(Aluno_Foto,Aluno_Nome, Aluno_Cidade, Aluno_Curso) VALUES ('".
     $nomeCompleto."','".$_POST
     ["campo_nome"]."','".$_POST
     ["campo_cidade"]."','".$_POST
     ["campo_curso"]."')");
-    //Ir para um determinado local
+    //por fim voltamos para os view's pelo header
     header("location:../view");
 ?>
